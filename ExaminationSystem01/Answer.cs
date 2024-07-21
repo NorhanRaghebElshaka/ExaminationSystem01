21964c980e19fddb04d21cfdb951cac06b81c875
norhan1522002 <norhan1522002@outlook.com>
2024-07-21 04:30:02 +0300

    Add project files.

---
+++ ExaminationSystem01/Answer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationSystem01
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}. {Text}\t\t\t";
+        }
+    }
+}
---
+++ ExaminationSystem01/Program.cs
@@ -0,0 +1,21 @@
+﻿using ExaminationSystem01;
+using ExaminationSystem01;
+using System.Diagnostics;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Subject subject1 = new Subject();
+        subject1.CreateExam();
+        Console.Clear();
+        Console.WriteLine("Do You Want To Start The Exame (y | n): ");
+        if (char.Parse(Console.ReadLine()) == 'y')
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            subject1.SubjectExam.DisplayExam();
+            Console.WriteLine($"The Elapsed Time = {stopwatch.Elapsed}");
+        }
+    }
+}
